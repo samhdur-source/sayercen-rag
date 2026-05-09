e8b8b38b85a57fec51b3ebe5495b05e5aa16b9d2
samhdur-source <samhdur@sayercen-d.com>
2026-05-09 11:06:48 -0600

    Replace HuggingFaceEmbeddings with GoogleGenerativeAIEmbeddings

--- rag_engine.py
+++ rag_engine.py
@@ -2,7 +2,7 @@ import os
 import glob
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
