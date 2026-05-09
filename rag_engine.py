7aa3375fef574d6b45460b92193dd8a32bbdebf0
samhdur-source <samhdur@sayercen-d.com>
2026-05-08 20:35:48 -0600

    fix: corregir import RecursiveCharacterTextSplitter

--- rag_engine.py
+++ rag_engine.py
@@ -15,7 +15,7 @@ import glob
 from typing import Optional
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
